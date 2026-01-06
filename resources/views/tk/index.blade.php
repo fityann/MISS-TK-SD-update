038ee08de452ef37861a19f335f3d75179784433
Arisssmndr <arisssmndrstudy.com>
2026-01-06 21:17:37 +0700

    Simpan kerjaan sebentar

--- resources/views/tk/index.blade.php
+++ resources/views/tk/index.blade.php
@@ -8,13 +8,12 @@
             Selamat Datang!
         </h1>
         <p class="mt-4 text-lg font-medium text-gray-800">
-            Yuk, belajar sambil bermain dan mengenal dunia TK Bersama Arisss!
+            Yuk, belajar sambil bermain dan mengenal dunia TK Bersama!
         </p>
     </div>
 
-    <div class="mt-12 max-w-5xl mx-auto text-2xl font-bold text-gray-900">Pembelajaran</div>
     {{-- Kategori belajar --}}
-    <div class="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-5xl mx-auto">
+    <div class="mt-18 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-5xl mx-auto">
 
         <a href="/paud/huruf"
             class="bg-yellow-300 hover:bg-yellow-400 transition transform hover:-translate-y-2 hover:rotate-1 duration-300 rounded-2xl p-6 text-center shadow-[0_10px_15px_rgba(0,0,0,0.2)]">
