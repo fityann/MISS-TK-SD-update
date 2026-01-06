6d9fcf981cb0fdc718c269aa0f017484f501a108
Arisssmndr <arisssmndrstudy.com>
2026-01-06 11:16:48 +0700

    update tampilan index tk

--- resources/views/tk/index.blade.php
+++ resources/views/tk/index.blade.php
@@ -19,7 +19,7 @@
         <a href="/paud/huruf"
             class="bg-yellow-300 hover:bg-yellow-400 transition transform hover:-translate-y-2 hover:rotate-1 duration-300 rounded-2xl p-6 text-center shadow-[0_10px_15px_rgba(0,0,0,0.2)]">
             <img src="{{ asset('assets/img/btn_abcs.png') }}" alt="Huruf" class="mx-auto w-20 mb-3 animate-wiggle">
-            <h2 class="text-2xl font-bold text-gray-900">Belajar Huruf</h2>
+            <h2 class="text-2xl font-bold text-gray-900">Matematika Dasar</h2>
             <p class="mt-2 text-sm text-gray-700">Kenali huruf dari A sampai Z!</p>
         </a>
 
@@ -27,14 +27,14 @@
             class="bg-blue-300 hover:bg-blue-400 transition transform hover:-translate-y-2 hover:rotate-1 duration-300 rounded-2xl p-6 text-center shadow-[0_10px_15px_rgba(0,0,0,0.2)]">
             <img src="{{ asset('assets/img/btn_numbers.png') }}" alt="Angka"
                 class="mx-auto w-20 mb-3 animate-wiggle">
-            <h2 class="text-2xl font-bold text-gray-900">Belajar Angka</h2>
+            <h2 class="text-2xl font-bold text-gray-900">Bahasa</h2>
             <p class="mt-2 text-sm text-gray-700">Hitung dari 1 sampai 10!</p>
         </a>
 
         <a href="/warna"
             class="bg-pink-300 hover:bg-pink-400 transition transform hover:-translate-y-2 hover:rotate-1 duration-300 rounded-2xl p-6 text-center shadow-[0_10px_15px_rgba(0,0,0,0.2)]">
             <img src="{{ asset('assets/img/btn_colors.png') }}" alt="Warna" class="mx-auto w-20 mb-3 animate-wiggle">
-            <h2 class="text-2xl font-bold text-gray-900">Belajar Warna</h2>
+            <h2 class="text-2xl font-bold text-gray-900">Agama & Moral</h2>
             <p class="mt-2 text-sm text-gray-700">Temukan warna-warna indah di sekitarmu!</p>
         </a>
     </div>
@@ -43,10 +43,10 @@
     <div class="mt-12 max-w-5xl mx-auto text-2xl font-bold text-gray-900">Quiz Pembelajaran</div>
     <div class="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-5xl mx-auto">
 
-        <a href="/paud/huruf/game"
+        <a href="/tk/matematika/game"
             class="bg-yellow-300 hover:bg-yellow-400 transition transform hover:-translate-y-2 hover:rotate-1 duration-300 rounded-2xl p-6 text-center shadow-[0_10px_15px_rgba(0,0,0,0.2)]">
             <img src="{{ asset('assets/img/btn_abcs.png') }}" alt="Huruf" class="mx-auto w-20 mb-3 animate-wiggle">
-            <h2 class="text-2xl font-bold text-gray-900">QuizBelajar Huruf</h2>
+            <h2 class="text-2xl font-bold text-gray-900">Matematika Dasar</h2>
             <p class="mt-2 text-sm text-gray-700">Kenali huruf dari A sampai Z!</p>
         </a>
 
@@ -54,7 +54,7 @@
             class="bg-blue-300 hover:bg-blue-400 transition transform hover:-translate-y-2 hover:rotate-1 duration-300 rounded-2xl p-6 text-center shadow-[0_10px_15px_rgba(0,0,0,0.2)]">
             <img src="{{ asset('assets/img/btn_numbers.png') }}" alt="Angka"
                 class="mx-auto w-20 mb-3 animate-wiggle">
-            <h2 class="text-2xl font-bold text-gray-900">Quiz Belajar Angka</h2>
+            <h2 class="text-2xl font-bold text-gray-900">Bahasa</h2>
             <p class="mt-2 text-sm text-gray-700">Hitung dari 1 sampai 10!</p>
         </a>
 
